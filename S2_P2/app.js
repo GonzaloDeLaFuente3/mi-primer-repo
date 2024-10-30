7207fc604567323415c27a9bca1a1fa0a430c9ed
De La Fuente Gonzalo <wwwgonzalito@hotmail.com>
2024-10-30 11:18:05 -0300

    Se subio el practico 2 del Sprint 2

---
+++ S2_P2/app.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://Grupo-04:grupo04@cursadanodejs.ls9ii.mongodb.net/Node-js',{
+    useNewUrlParser: true,
+    useUnifiedTopology:true
+}
+)
+.then(()=> console.log('Conexion exitosa a MongoDB'))
+.catch(error => console.error('Error al conectar a MongoDB:', error));
+
+
+//esquema para los superheroes
+const superheroSchema = new mongoose.Schema({
+    nombreSuperHeroe: {type:String , required:true},
+    nombreReal:{type:String, required:true} ,
+    edad : {type:Number, min:0},
+    planetaOrigen: {type: String, default:'Desconocido'},
+    debilidad : String,
+    poderes: [String],
+    aliados:[String],
+    enemigos: [String ],
+    createdAt: {type:Date, default: Date.now}
+    }, { collection: 'Grupo-04' });// Aquí defino la coleccion del grupo 
+
+const SuperHero = mongoose.model('SuperHero', superheroSchema);
+
+
+//funcion para insertar un nuevo superheroe 
+async function insertSuperHero() {
+    const hero = new SuperHero({
+        nombreSuperHeroe:'Goku',
+        nombreReal:'Kakarotto',
+        edad:43,
+        planetaOrigen: 'Planeta Vegeta',
+        debilidad: 'Inocencia y confianza excesiva',
+        poderes: ['Super Saiyan',
+        'Kamehameha',
+        'Vuelo',
+        'Teletransportación'],
+        aliados: ['Vegeta',
+        'Gohan',
+        'Piccolo'],
+        enemigos: ['Freezer',
+        'Cell',
+        'Majin Buu']
+    });
+    await hero.save();
+    console.log('Superheroe insertado', hero);
+}
+
+insertSuperHero();
+
+//actualizar superheroe
+async function updateSuperHero(nombreSuperHeroe) {
+    const result = await SuperHero.updateOne(
+        {nombreSuperHeroe: nombreSuperHeroe},
+        {$set:{edad:50}}
+    );
+    console.log('Resultado de la actualizacion:', result);
+    
+
+}
+
+updateSuperHero('Goku');
+
+//eliminar superheroe de la coleccion 
+async function deleteSuperHero(nombreSuperHeroe) {
+    const result = await SuperHero.deleteOne({nombreSuperHeroe:nombreSuperHeroe});
+    console.log('Superheroe eliminado:', result);
+
+    
+}
+
+deleteSuperHero('Goku');
+
+
+//funcion para buscar superheroes segun su planeta de origen 
+async function findSuperHeroes() {
+    const heroes = await SuperHero.find({planetaOrigen: 'Tierra'});
+    console.log('Superheroes encontrados:', heroes);
+    console.log('Cantidad de superhéroes encontrados:', heroes.length);
+}
+
+findSuperHeroes();
